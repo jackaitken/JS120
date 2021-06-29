5949818cab59da91f374b0ae3b176613623bb026
Jack Aitken <jaitk11@gmail.com>
2021-06-29 16:41:16 -0400

    inital commit

---
+++ lesson_1/object_oriented_rps.js
@@ -0,0 +1,27 @@
+function createPlayer() {
+  return {
+    // player's name?
+    // player's current move
+
+    choose() {
+      // pass
+    },
+  };
+}
+
+function createMove() {
+  return {
+    // (possible state: type of move (rock, paper, scissors))
+  };
+}
+
+function createRule() {
+  return {
+    // (possible state: unsure if Rules need state)
+  };
+}
+
+function compare(move1, move2) {
+  // to implement
+}
+
