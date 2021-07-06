76dc4324780f214a3ca0b57120679b15f962fba2
Jack Aitken <jaitk11@gmail.com>
2021-07-06 17:39:04 -0400

    Execution context exercises

--- lesson_1/practice.js
+++ lesson_1/practice.js
@@ -1,28 +1,64 @@
-let greetings = {
-  morning: 'Good morning, ',
-  afternoon: 'Good afternoon, ',
-  evening: 'Good evening, ',
-
-  greeting: function(name) {
-    let currentHour = (new Date()).getHours();
-
-    if (currentHour < 12) {
-      console.log(this.morning + name);
-    } else if (currentHour < 18) {
-      console.log(this.afternoon + name);
-    } else {
-      console.log(this.evening + name);
-    }
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(function(title) {
+      console.log(this.seriesTitle + ': ' + title);
+    });
   }
 };
 
-let spanishWords = {
-  morning: 'Buenos dias, ',
-  afternoon: 'Buenas tardes, ',
-  evening: 'Buena noches, '
+TESgames.listGames();
+
+/*
+Will output:
+undefined: Arena
+undefined: Daggerfall
+undefined: Morrowind
+undefined: Oblivion
+undefined: Skyrim
+
+To fix we can do this:
+*/
+
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    let self = this;
+    this.titles.forEach(function(title) {
+      console.log(self.seriesTitle + ': ' + title);
+    });
+  }
 };
 
-let spanishGreeter = greetings.greeting.bind(spanishWords);
+TESgames.listGames();
 
-spanishGreeter('Jose');
-spanishGreeter('Juan');
+// Or:
+
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(function(title) {
+      console.log(this.seriesTitle + ': ' + title);
+    }, this);
+  }
+};
+
+TESgames.listGames();
+
+// Or
+
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(title => {
+      console.log(this.seriesTitle + ': ' + title);
+    });
+  }
+};
+
+TESgames.listGames();
+
