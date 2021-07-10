e8566eb6c4d8e5c0b3d5037154431890720cc43a
Jack Aitken <jaitk11@gmail.com>
2021-07-10 13:37:40 -0400

    Working with constructors

--- lesson_3/practice.js
+++ lesson_3/practice.js
@@ -1,60 +1,19 @@
-function createInvoice(services = {}) {
-  return {
-    phonePrice: services.phone || 3000,
-    internetPrice: services.internet || 5500,
-    payments: [],
-    total: function() {
-      return this.phonePrice + this.internetPrice;
-    },
-    
-    addPayment: function(services = {}) {
-      this.payments.push(services)
-    },
-
-    addPayments: function(servicesArr) {
-      servicesArr.forEach(this.addPayment, this);
-    },
-
-    totalPayment: function() {
-      return this.payments.reduce((acc, elem) => acc + elem.total(), 0);
-    },
-
-    amountDue: function() {
-      return this.total() - this.totalPayment();
-    },
-  }
+function Dog(name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
 }
 
-function createPayment(services = {}) {
-  return {
-    phonePayment: services.phone || 0,
-    internetPayment: services.internet || 0,
-    amount: services.amount || 0,
-
-    total: function() {
-      if (this.phonePayment > 0 || this.internetPayment > 0) {
-        return this.phonePayment + this.internetPayment;
-      } else {
-        return this.amount;
-      }
-    }
-  }
+Dog.prototype.bark = function() {
+  console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
 }
 
-let invoice = createInvoice({
-  phone: 1200,
-  internet: 4000,
-});
+let maxi = new Dog('Maxi', 'German Shepherd', 32);
+let dexter = new Dog('Dexter', 'Rottweiler', 50);
 
-let payment1 = createPayment({ amount: 2000 });
-let payment2 = createPayment({
-  phone: 1000,
-  internet: 1200
-});
-
-let payment3 = createPayment({ phone: 1000 });
-
-invoice.addPayment(payment1);
-invoice.addPayments([payment2, payment3]);
-console.log(invoice.amountDue());       // this should return 0
-console.log(invoice.payments);
+dexter.bark = function() {
+  console.log('WOOF!');
+}
+maxi.bark(); // Woof!
+dexter.bark(); // WOOF!
+console.log(Object.getPrototypeOf(dexter).bark());
