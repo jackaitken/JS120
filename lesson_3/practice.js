555bd0ce185bd458925aac38a843c7f060accd58
Jack Aitken <jaitk11@gmail.com>
2021-07-11 12:37:23 -0400

    Using the class keyword

--- lesson_3/practice.js
+++ lesson_3/practice.js
@@ -1,15 +1,33 @@
-function User(first, last) {
-  if (this instanceof User) {
-    this.name = first + ' ' + last;
-  } else {
-    return new User(first, last);
+function Rectangle(length, width) {
+  this.length = length;
+  this.width = width;
+}
+
+Rectangle.prototype.getArea = function() {
+  return this.length * this.width;
+}
+
+let rec = new Rectangle(10, 5);
+console.log(typeof Rectangle);         // function
+console.log(rec instanceof Rectangle); // true
+console.log(rec.constructor);          // [Function: Rectangle]
+console.log(rec.getArea());            // 50
+
+// Now the same thing but with 'class'
+
+class Rectangle {
+  constructor(length, width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  getArea() {
+    return this.length * this.width;
   }
 }
 
-let name = 'Jane Doe';
-let user1 = new User('John', 'Doe');
-let user2 = User('John', 'Doe');
-
-console.log(name);         // => Jane Doe
-console.log(user1.name);   // => John Doe
-console.log(user2.name);   // => John Doe
+let rec = new Rectangle(10, 5);
+console.log(typeof Rectangle);         // function
+console.log(rec instanceof Rectangle); // true
+console.log(rec.constructor);          // [Function: Rectangle]
+console.log(rec.getArea());            // 50
