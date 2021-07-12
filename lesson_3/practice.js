b3fb9fcc39b355501232ffb5a49aec99cdef5237
Jack Aitken <jaitk11@gmail.com>
2021-07-12 09:09:30 -0400

    Factory Functions vs. OLOO

--- lesson_3/practice.js
+++ lesson_3/practice.js
@@ -1,33 +1,7 @@
-function Rectangle(length, width) {
-  this.length = length;
-  this.width = width;
-}
-
-Rectangle.prototype.getArea = function() {
-  return this.length * this.width;
-}
-
-let rec = new Rectangle(10, 5);
-console.log(typeof Rectangle);         // function
-console.log(rec instanceof Rectangle); // true
-console.log(rec.constructor);          // [Function: Rectangle]
-console.log(rec.getArea());            // 50
-
-// Now the same thing but with 'class'
-
-class Rectangle {
-  constructor(length, width) {
-    this.length = length;
-    this.width = width;
-  }
-
-  getArea() {
-    return this.length * this.width;
+class A {
+  constructor(param) {
+    this.param = param;
   }
 }
 
-let rec = new Rectangle(10, 5);
-console.log(typeof Rectangle);         // function
-console.log(rec instanceof Rectangle); // true
-console.log(rec.constructor);          // [Function: Rectangle]
-console.log(rec.getArea());            // 50
+console.log(typeof A);
