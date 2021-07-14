27f54ae1f2cd5930cfcaa7a39957cd8164cbc676
Jack Aitken <jaitk11@gmail.com>
2021-07-14 08:01:38 -0400

    initial commit, added spike for play() in TTTGame

---
+++ lesson_5/oo_ttt.js
@@ -0,0 +1,64 @@
+class Board {
+  constructor() {
+    
+  }
+}
+
+class Square {
+  constructor() {
+    
+  }
+}
+
+class Row {
+  constructor() {
+    
+  }
+}
+
+class Marker {
+  constructor() {
+    
+  }
+}
+
+class Player {
+  constructor() {
+    
+  }
+
+  mark() {
+
+  }
+
+  play() {
+
+
+  }
+}
+
+
+class Human extends Player {
+  constructor() {
+    
+  }
+}
+
+class Computer extends Player {
+  constructor() {
+    
+  }
+}
+
+class TTTGame {
+  constructor() {
+
+  }
+
+  play() {
+
+  }
+}
+
+let game = new TTTGame();
+game.play();
