8cc54471a004ec921ea6e410f52290877c1ab763
Jack Aitken <jaitk11@gmail.com>
2021-07-14 08:11:13 -0400

    Added welcome and goodbye messages

--- lesson_5/oo_ttt.js
+++ lesson_5/oo_ttt.js
@@ -1,62 +1,107 @@
 class Board {
   constructor() {
-    
+    // STUB
   }
 }
 
 class Square {
   constructor() {
-    
+    // STUB
   }
 }
 
 class Row {
   constructor() {
-    
+    // STUB
   }
 }
 
 class Marker {
   constructor() {
-    
+    // STUB
   }
 }
 
 class Player {
   constructor() {
-    
+    // STUB
   }
 
   mark() {
-
+    // STUB
   }
 
   play() {
-
-
+    // STUB
   }
 }
 
 
 class Human extends Player {
   constructor() {
-    
+    // STUB
   }
 }
 
 class Computer extends Player {
   constructor() {
-    
+    // STUB
   }
 }
 
 class TTTGame {
   constructor() {
-
+    // STUB
   }
 
   play() {
+    this.displayWelcomeMessage();
 
+    while (true) {
+      this.displayBoard();
+
+      this.firstPlayerMoves();
+      if (this.gameOver()) break;
+
+      this.secondPlayerMoves();
+      if (this.gameOver()) break;
+    }
+
+    this.displayResults();
+    this.displayGoodbyeMessage();
+  }
+
+  displayWelcomeMessage() {
+    console.log('Welcome to Tic Tac Toe!');
+  }
+
+  displayGoodbyeMessage() {
+    console.log('Thanks for playing Tic Tact Toe! Goodbye!');
+  }
+
+  displayResults() {
+    // STUB
+    // Show the results of the game (win, tie, loss)
+  }
+
+  displayBoard() {
+    // STUB
+    // Display the board and its current state
+  }
+
+  firstPlayerMoves() {
+    // STUB
+    // the first player makes a move
+  }
+
+  secondPlayerMoves() {
+    // STUB
+    // the second player makes a move
+  }
+
+  gameOver() {
+    // STUB
+    return false;
   }
 }
 
