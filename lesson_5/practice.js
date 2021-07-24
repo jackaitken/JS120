45c5d3d0b66f51bf9a67492fa545374256d017ad
Jack Aitken <jaitk11@gmail.com>
2021-07-24 09:41:49 -0400

    BST

--- lesson_5/practice.js
+++ lesson_5/practice.js
@@ -10,6 +10,32 @@ class Tree {
   constructor(root) {
     this.root = root;
   }
+
+  search(targetValue, node) {
+    if (node === null || targetValue === node.data) {
+      return node;
+    } else if (targetValue < node.data) {
+      return this.search(targetValue, node.left);
+    } else {
+      return this.search(targetValue, node.right);
+    }
+  }
+
+  insert(value, node) {
+    if (value < node.data) {
+      if (node.left === null) {
+        node.left = new TreeNode(value);
+      } else {
+        this.insert(value, node.left);
+      }
+    } else if (value > node.data) {
+      if (node.right === null) {
+        node.right = new TreeNode(value);
+      } else {
+        this.insert(value, node.right);
+      }
+    }
+  }
 }
 
 let node1 = new TreeNode(10);
@@ -17,4 +43,9 @@ let node2 = new TreeNode(20);
 let root = new TreeNode(15, node1, node2);
 let tree1 = new Tree(root);
 
-console.log(tree1);
+for (let i = 0; i < 1000; i ++) {
+  let randNum = Math.floor(Math.random() * 100);
+  tree1.insert(randNum, root);
+}
+
+console.log(tree1.search(32, root));
