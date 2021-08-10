211c8ef6e61c8d77a6b4dd2c38f2f4a8ed7c4971
Jack Aitken <jaitk11@gmail.com>
2021-08-09 22:44:39 -0400

    interview prep

--- practice.js
+++ practice.js
@@ -1,96 +1,23 @@
-/*
-You have recently watched Lion King as well as Jungle Book and have decided to take the best
-of both these worlds and develop some OOP magic. In this Lion King x Jungle Book world of ours, 
-Lions have two characteristics:
-1) they are friendly;
-2) they are in the Lion King
-Lions also have certain behavior, namely they roar and say 'Hakuna Matata'
-
-Tigers, likewise, have two characteristics:
-1) they enjoy swimming;
-2) they are in the Jungle Book
-Tigers also have certain behavior, namely they chase and say 'I am chasing the man-cub'
-
-Your task is to create a Liger (https://en.wikipedia.org/wiki/Liger).
-A Liger is an animal whose father is a Lion and whose mother is a Tiger. 
-The liger you create should inherit ALL properties and functionality from its parents, as follows:
-
-console.log(liger.roar()) // 'Hakuna Matata'
-console.log(liger.chase()) // 'I am chasing the man-cub'
-console.log(liger.inLionKing) // true
-console.log(liger.isFriendly) // true
-console.log(liger.enjoysSwimming) // true
-console.log(liger.inJungleBook) // true
-
-Please construct this relationship using the following patterns:
-1) Constructor Prototypes
-2) OLOO
-3) Factory functions
-
-How you set up the relationship between the child liger, and parent Lion and Tiger is up to you.
-*/
-
-// let TigerBehavior = {
-//   chase() {
-//     return 'I am chasing the man-cub';
-//   }
-// }
-
-// function Lion() {
-//   this.isFriendly = true;
-//   this.inLionKing = true;
-// }
-
-// Lion.prototype.roar = function() {
-//     return 'Hakuna Matata';
-// }
-
-// function Tiger() {
-//   this.enjoysSwimming = true;
-//   this.inJungleBook = true;
-// }
-
-// function Liger() {
-//   Lion.call(this);
-//   Tiger.call(this);
-// }
-
-// Liger.prototype = Object.create(Lion.prototype);
-// Object.assign(Liger.prototype, TigerBehavior);
-
-// let liger = new Liger();
-
-
-let LionMixin = {
-  isFriendly: true,
-  inLionKing: true,
-
-  roar() {
-    return 'Hakuna Matata';
+class Node {
+  constructor(data, leftChild = null, rightChild = null) {
+    this.data = data;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
   }
 }
 
-let ligerPrototype = {
-  init() {
-    this.enjoysSwimming = true;
-    this.inJungleBook = true;
-    return this;
-  },
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-  chase() {
-    return 'I am chasing the man-cub';
+  insert(data) {
+    let node = new Node(data);
+    if (this.root === null) {
+      this.root === data;
+    } else if ()
   }
 }
 
-Object.assign(ligerPrototype, LionMixin);
-
-let liger = Object.create(ligerPrototype).init();
-
-
-
-console.log(liger.roar()) // 'Hakuna Matata'
-console.log(liger.chase()) // 'I am chasing the man-cub'
-console.log(liger.inLionKing) // true
-console.log(liger.isFriendly) // true
-console.log(liger.enjoysSwimming) // true
-console.log(liger.inJungleBook) // true
+let node1 = new Node(15);
+console.log(node1);
---
+++ practice1.js
@@ -0,0 +1,7 @@
+let randomValue;
+let min = 1;
+let max = 13;
+
+while (randomValue != 12) {
+  randomValue = Math.floor(Math.random() * (max - min) + min);
+}
