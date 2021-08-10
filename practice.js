f0345e05754efd57d1d36725e4dc5a2bb2269edd
Jack Aitken <jaitk11@gmail.com>
2021-08-10 08:51:30 -0400

    interview prep

--- practice.js
+++ practice.js
@@ -7,18 +7,26 @@ let WaterDrivable = {
 }
 
 class Plane {
+  constructor() {
+    Plane.allPlanes.push(this);
+  }
+
   fly() {
     console.log('Flying');
   }
+
+  getAllPlanes() {
+    console.log('---Planes that have been made---');
+    Plane.allPlanes.forEach((plane, index) => {
+      console.log(`${index + 1}: ${plane.constructor.name}`);
+    });
+  }
 }
+Plane.allPlanes = [];
 
 class Jet extends Plane {}
 
-class Boat {
-  driveOnWater() {
-    console.log('Driving on Water');
-  }
-}
+class Boat {}
 
 class MotorBoat extends Boat {}
 
@@ -26,4 +34,6 @@ class SeaPlane extends Plane {}
 Object.assign(SeaPlane.prototype, WaterDrivable);
 
 let plane = new SeaPlane();
-plane.driveOnWater();
+let plane2 = new Jet();
+
+plane.getAllPlanes();
