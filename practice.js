7b1e51b3b2df43c2706d4567fe74369aa2b520b0
Jack Aitken <jaitk11@gmail.com>
2021-08-13 18:19:18 -0400

    interview prep

--- practice.js
+++ practice.js
@@ -1,19 +1,92 @@
+
+'use strict';
 /*
-1. What is OLOO and how does is differ from the constructor/prototype pattern?
+Memory Dump
+  S.  J. 
+- [x] [x] object-oriented programming 
+- [x] [x] The `new` keyword 
+- [x] [x] static properties and methods 
+- [x] [x] mixins 
+- [x] [x] inheritance 
+- [x] [x] polymorphism 
+- [x] [x] encapsulation
+- [x] [x] factory functions
+- [x] [x] OLOO 
+- [] [x] Property sequence lookup 
+- [] [x] Prototype chain 
+- [] [] constructors
+- [] [] The private `[[Prototype]]` property 
+- [] [] Collaborator objects
+- [] [] Execution context and the value of `this`
+- [] [] Context Loss
+- [] [] The global object
+- [] [] Higher-order functions 
+- [] [] Invoking methods on a primitive value 
+- [] [] `instanceof` vs `typeof`
+- [] [] `constructor` property
+- [] [] different ways we can check an object come from constructor function 
 
-2. Explain using an example what the bind method is and how it works.
-    a. How does it differ from the call and apply methods?
+NOTES 
+- property sequence lookup and prototype chain are interelated 
 
-3. Using an example explain the differences between prototypal and pseudo-classical inheritance
-
-4. 
 */
 
-// 3
 
-function greet() {
-  let firstName = 'Sergio';
-  return `${this}`;   // TypeError
+/*
+Mix in example
+Polymorphism example
+constructor/prototype example
+class example
+OLOO example
+factory function example
+Prototypal inheritance
+Pseudo-Classical inheritance
+*/
+
+// Mix - in
+
+/*
+let WaterDriveable = {
+  driveOnWater() {
+    console.log('Driving on water');
+  }
 }
 
-console.log(greet()); // "Sergio"
+class Plane {
+  fly() {
+    console.log('Flying');
+  }
+}
+
+class Jet extends Plane {}
+
+class SeaPlane extends Plane {}
+Object.assign(SeaPlane.prototype, WaterDriveable);
+
+class MotorBoat {}
+Object.assign(MotorBoat.prototype, WaterDriveable);
+*/
+
+// Polymorphism
+
+class Wrench {
+  info() {
+    console.log("I'm a wrench!");
+  }
+}
+
+class Tree {
+  info() {
+    console.log("I'm a tree!");
+  }
+}
+
+class Person {
+  info() {
+    console.log("I'm a person!");
+  }
+}
+
+[new Wrench(), new Tree(), new Person()].forEach(obj => {
+  obj.info();
+});
